7e8c3c4a63044ee906de165492f49ab4233c4f5a
高威威 <2950814933@qq.com>
2022-11-15 04:47:12 +0800

    '完善注册页js'

--- js/register.js
+++ js/register.js
@@ -99,7 +99,7 @@ function setRem() {
     var client_w = document.documentElement.clientWidth || document.body.clientWidth;
     //设置最大最小宽度
     client_w = client_w > 625 ? 625 : client_w;
-    client_w = client_w < 300 ? 300 : client_w;
+    client_w = client_w < 100 ? 100 : client_w;
     // console.log(ui_w, client_w);
     var html = document.getElementsByTagName("html")[0];
     html.style.fontSize = (client_w / ui_w) * 10 + 'px';
