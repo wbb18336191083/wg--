08ce07782a74387c37742df548d4109fa95c6742
高威威 <2950814933@qq.com>
2022-11-15 03:57:48 +0800

    '修改注册js样式'

--- js/register.js
+++ js/register.js
@@ -8,7 +8,8 @@ var s2 = parseInt(Math.random() * 10);
 var s3 = parseInt(Math.random() * 10);
 var s4 = parseInt(Math.random() * 10);
 var s5 = s1 * 1000 + s2 * 100 + s3 * 10 + s4;
-function fn() {
+
+touch.on(verification, 'tap', function () {
     clearInterval(time);
     time = setInterval(function () {
         num--;
@@ -28,8 +29,8 @@ function fn() {
 
     }, 1000)
 
-}
-verification.onclick = fn;
+})
+
 
 /* 正则表达式 */
 var telspan = document.getElementById('telspan');
@@ -94,31 +95,32 @@ register.onclick = function () {
 /* set rem */
 var timer = null;
 function setRem() {
-    var ui_w = 640;
+    var ui_w = 1349;
     var client_w = document.documentElement.clientWidth || document.body.clientWidth;
     //设置最大最小宽度
-    client_w = client_w > 600 ? 600 : client_w;
-    client_w = client_w < 250 ? 250 : client_w;
+    // client_w = client_w > 625 ? 625 : client_w;
+    // client_w = client_w < 300 ? 300 : client_w;
     // console.log(ui_w, client_w);
     var html = document.getElementsByTagName("html")[0];
     html.style.fontSize = (client_w / ui_w) * 10 + 'px';
 }
 
 //防抖
-window.onresize = function () {
-    clearTimeout(timer);
-    timer = setTimeout(setRem, 500)
-}
+// window.onresize = function () {
+//     clearTimeout(timer);
+//     timer = setTimeout(setRem, 500)
+// }
 
 //节流
-// var flg = true;
-// window.onresize = function () {
-//     if (flg) {
-//         flg = false;
-//         setTimeout(function () {
-//             flg = true;
-//             setRem();
-//         }, 300)
-//     }
-// }
+var flg = true;
+window.onresize = function () {
+    if (flg) {
+        flg = false;
+        setTimeout(function () {
+            flg = true;
+            setRem();
+        }, 300)
+    }
+}
 window.onload = setRem;
+window.onresize = setRem;
--- js/rentalDetails.js
+++ js/rentalDetails.js
@@ -1,1 +1,31 @@
-console.log('哈哈哈');
+/* set rem */
+var timer = null;
+function setRem() {
+    var ui_w = 640;
+    var client_w = document.documentElement.clientWidth || document.body.clientWidth;
+    //设置最大最小宽度
+    client_w = client_w > 600 ? 600 : client_w;
+    client_w = client_w < 250 ? 250 : client_w;
+    // console.log(ui_w, client_w);
+    var html = document.getElementsByTagName("html")[0];
+    html.style.fontSize = (client_w / ui_w) * 10 + 'px';
+}
+
+//防抖
+window.onresize = function () {
+    clearTimeout(timer);
+    timer = setTimeout(setRem, 500)
+}
+
+//节流
+// var flg = true;
+// window.onresize = function () {
+//     if (flg) {
+//         flg = false;
+//         setTimeout(function () {
+//             flg = true;
+//             setRem();
+//         }, 300)
+//     }
+// }
+window.onload = setRem;
