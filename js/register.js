bf46f151b9a462c62d942485bff10dbf71ebe1ab
高威威 <2950814933@qq.com>
2022-11-14 22:01:18 +0800

    '修改注册js'

--- js/register.js
+++ js/register.js
@@ -94,30 +94,31 @@ register.onclick = function () {
 /* set rem */
 var timer = null;
 function setRem() {
-    var ui_w = 375;
+    var ui_w = 640;
     var client_w = document.documentElement.clientWidth || document.body.clientWidth;
     //设置最大最小宽度
-    client_w = client_w > 625 ? 625 : client_w;
-    client_w = client_w < 300 ? 300 : client_w;
+    client_w = client_w > 600 ? 600 : client_w;
+    client_w = client_w < 250 ? 250 : client_w;
     // console.log(ui_w, client_w);
     var html = document.getElementsByTagName("html")[0];
     html.style.fontSize = (client_w / ui_w) * 10 + 'px';
 }
 
 //防抖
-// window.onresize = function () {
-//     clearTimeout(timer);
-//     timer = setTimeout(setRem, 500)
-// }
+window.onresize = function () {
+    clearTimeout(timer);
+    timer = setTimeout(setRem, 500)
+}
 
 //节流
-var flg = true;
-window.onresize = function () {
-    if (flg) {
-        flg = false;
-        setTimeout(function () {
-            flg = true;
-            setRem();
-        }, 300)
-    }
-}
+// var flg = true;
+// window.onresize = function () {
+//     if (flg) {
+//         flg = false;
+//         setTimeout(function () {
+//             flg = true;
+//             setRem();
+//         }, 300)
+//     }
+// }
+window.onload = setRem;
